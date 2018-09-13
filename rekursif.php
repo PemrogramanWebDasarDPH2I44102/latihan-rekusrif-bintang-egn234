76c6a5a697b3f7fcca9f571067dae70f11fa0b6e
Egan Kusmaya Putra <42627926+egn234@users.noreply.github.com>
2018-09-13 08:25:27 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+  $batas
+  function rekursif($i = 1){
+    global $batas;
+    echo "*";
+    $i++;
+    if($i<=$batas){
+      rekursif($i);
+    }
+    else{
+      echo "<br>";
+      $batas--;
+      if($batas != 0){
+        rekursif();
+      }
+    }
+  }
+  rekursif();
+  echo "<br>";
+/*====================================================================================*/
+
+  $batas2 = 1;
+  function rekursif2($t = 1){
+    global $batas2;
+    
+    echo "$t";
+    $t++;
+    
+    if($t<=$batas2){
+      rekursif2($t);
+    }
+    else{
+      echo "<br>";
+      $batas2++;
+      if($batas2<=5){
+        rekursif2();
+      }
+    }
+  }
+  rekursif2();
+    
 ?>
